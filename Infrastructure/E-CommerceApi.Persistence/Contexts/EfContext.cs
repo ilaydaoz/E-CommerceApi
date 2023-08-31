ee947ac769d629b3a57ee4beac8b0f72bbe23904
ilayda <ilaydaozken@gmail.com>
2023-08-31 16:12:55 +0300

    db bağlantı

---
+++ Infrastructure/E-CommerceApi.Persistence/Contexts/EfContext.cs
@@ -0,0 +1,15 @@
+﻿using E_CommerceApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceApi.Persistence.Contexts
+{
+    public class EfContext : DbContext
+    {
+        public EfContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet <Product> Products { get; set; }
+        public DbSet <Order> Orders { get; set; }
+        public DbSet <Customer> Customers { get; set; }
+    }
+}
---
+++ Infrastructure/E-CommerceApi.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using E_CommerceApi.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace E_CommerceApi.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EfContext>(x =>
+            {
+                x.UseNpgsql(configuration.GetConnectionString("EfDbContext"));
+            });
+            services.AddScoped<DbContext, EfContext>();
+        }
+    }
+}
